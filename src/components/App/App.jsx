b78cb0e3b263273d318512002f87c5459ec04c70
Emil Nowak <emil32132@onet.pl>
2023-07-15 21:03:52 +0200

    Add base and library

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,34 @@
+import { Routes, NavLink, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import CanvasAnimation from '../CanvasAnimation/CanvasAnimation';
 import Header from 'components/Header/Header';
+import Home from 'pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import NotFound from 'pages/NotFound/NotFound';
 
 export const App = () => {
+  const StyledLink = styled(NavLink)`
+    color: black;
+
+    &.active {
+      color: orange;
+    }
+  `;
   return (
     <div>
       <CanvasAnimation>
-        <div>
+        <nav>
           <Header></Header>
-        </div>
+          <StyledLink></StyledLink>
+          <StyledLink></StyledLink>
+          <StyledLink></StyledLink>
+        </nav>
       </CanvasAnimation>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+
+    
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default NotFound;
